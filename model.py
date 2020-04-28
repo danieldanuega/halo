f4bec81f3a24fb3c02dcc7507de9a63c7774cf09
Daniel Go <godaniel135@gmail.com>
2020-04-29 01:10:39 +0700

    Face recognition

---
+++ model.py
@@ -0,0 +1,21 @@
+from tensorflow.keras import models
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, BatchNormalization, Dropout
+from tensorflow.keras import regularizers
+
+def get_model():
+    model = models.Sequential()
+    model.add(Conv2D(16, kernel_size=3, activation='relu', kernel_regularizers=regularizers.l2(0.), activity_regularizer=regularizers.l2(0.), input_shape=(224, 224, 3)))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.05))
+    model.add(Conv2D(32, kernel_size=3, activation='relu',
+                     kernel_regularizer=regularizers.l2(0.),
+                     activity_regularizer=regularizers.l2(0.)))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.05))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu', # 3000
+                    kernel_regularizer=regularizers.l2(0.),
+                    activity_regularizer=regularizers.l2(0.)))
+    model.add(Dropout(0.05))
