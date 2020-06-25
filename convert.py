a806b6590333512799bfb9507431bfb7eaa71c30
danielgo13 <godaniel135@gmail.com>
2020-06-25 15:45:46 +0700

    Convert keras to tensorrt model

---
+++ convert.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+tf.keras.backend.set_learning_phase(0) #use this if we have batch norm layer in our network
+from tensorflow.keras.models import load_model
+from tensorflow.python.platform import gfile
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+
+# Save into tensorflow model
+tf.compat.v1.disable_eager_execution()
+model = load_model('./models/keras_deepface.h5')
+saver = tf.compat.v1.train.Saver()
+sess = tf.compat.v1.keras.backend.get_session()
+save_path = saver.save(sess, './models/tfdeepface')
+
+with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.50))) as sess:
+    # import the meta graph of the tensorflow model
+    #saver = tf.train.import_meta_graph("./model/tensorflow/big/model1.meta")
+    saver = tf.compat.v1.train.import_meta_graph("./models/tfdeepface.meta")
+    # then, restore the weights to the meta graph
+    #saver.restore(sess, "./model/tensorflow/big/model1")
+    saver.restore(sess, "./models/tfdeepface")
+    
+    # specify which tensor output you want to obtain 
+    # (correspond to prediction result)
+    your_outputs = ["F7/Relu"]
+    
+    # convert to frozen model
+    frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(
+        sess, # session
+        tf.compat.v1.get_default_graph().as_graph_def(),# graph+weight from the session
+        output_node_names=your_outputs)
+    #write the TensorRT model to be used later for inference
+    with gfile.GFile("./models/frozen_deepface.pb", 'wb') as f:
+        f.write(frozen_graph.SerializeToString())
+    print("Frozen model is successfully stored!")
+
+    # convert (optimize) frozen model to TensorRT model
+    trt_graph = trt.create_inference_graph(
+        input_graph_def=frozen_graph,# frozen model
+        outputs=your_outputs,
+        max_batch_size=1,# specify your max batch size
+        max_workspace_size_bytes=2*(10**9),# specify the max workspace
+        precision_mode="FP16") # precision, can be "FP32" (32 floating point precision) or "FP16"
+
+    #write the TensorRT model to be used later for inference
+    with gfile.GFile("deepface_tensorrt.pb", 'wb') as f:
+        f.write(trt_graph.SerializeToString())
+    print("TensorRT model is successfully stored!")
--- model.py
+++ model.py
@@ -4,47 +4,46 @@ import os
 from pathlib import Path
 import zipfile
 import gdown
+import tensorflow as tf
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+from tensorflow.python.platform import gfile
+from tensorflow.keras.models import load_model
 
-def get_input_shape():
-    return (152,152)
+model = Sequential()
 
-def load_FbDeepFace():
-    model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(11,11), activation='relu', name='C1', input_shape=(152,152,3)))
+model.add(MaxPool2D(pool_size=(3,3), strides=2, padding='same', name='M2'))
+model.add(Conv2D(filters=16, kernel_size=(9,9), activation='relu', name='C3'))
+
+model.add(LocallyConnected2D(filters=16, kernel_size=(9,9), activation='relu', name='L4'))
+model.add(LocallyConnected2D(filters=16, kernel_size=(7,7), strides=2, activation='relu', name='L5'))
+model.add(LocallyConnected2D(filters=16, kernel_size=(5,5), activation='relu', name='L6'))
+
+model.add(Flatten(name='F0'))
+
+model.add(Dense(4096, activation='relu', name='F7'))
+model.add(Dropout(rate=0.5, name='D0'))
+model.add(Dense(8631, activation='softmax', name='F8'))
+
+home = str(Path.home())
+
+model_path = home + '/.deepface/weights/VGGFace2_DeepFace_weights_val-0.9034.h5'
+
+if os.path.isfile(model_path) != True:
+    print("DeepFace weights by will be downloaded ")
+    os.makedirs(home + '/.deepface/weights')
     
-    model.add(Conv2D(filters=32, kernel_size=(11,11), activation='relu', name='C1', input_shape=(152,152,3)))
-    model.add(MaxPool2D(pool_size=(3,3), strides=2, padding='same', name='M2'))
-    model.add(Conv2D(filters=16, kernel_size=(9,9), activation='relu', name='C3'))
+    url = 'https://github.com/swghosh/DeepFace/releases/download/weights-vggface2-2d-aligned/VGGFace2_DeepFace_weights_val-0.9034.h5.zip'
     
-    model.add(LocallyConnected2D(filters=16, kernel_size=(9,9), activation='relu', name='L4'))
-    model.add(LocallyConnected2D(filters=16, kernel_size=(7,7), strides=2, activation='relu', name='L5'))
-    model.add(LocallyConnected2D(filters=16, kernel_size=(5,5), activation='relu', name='L6'))
+    output = model_path + '.zip'
     
-    model.add(Flatten(name='F0'))
+    gdown.download(url, output, quiet=False)
     
-    model.add(Dense(4096, activation='relu', name='F7'))
-    model.add(Dropout(rate=0.5, name='D0'))
-    model.add(Dense(8631, activation='softmax', name='F8'))
-    
-    home = str(Path.home())
-    
-    model_path = home + '/.deepface/weights/VGGFace2_DeepFace_weights_val-0.9034.h5'
-    
-    if os.path.isfile(model_path) != True:
-        print("DeepFace weights by will be downloaded ")
-        os.makedirs(home + '/.deepface/weights')
+    with zipfile.ZipFile(output, 'r') as z:
+        z.extractall(home + '/.deepface/weights')
         
-        url = 'https://github.com/swghosh/DeepFace/releases/download/weights-vggface2-2d-aligned/VGGFace2_DeepFace_weights_val-0.9034.h5.zip'
-        
-        output = model_path + '.zip'
-        
-        gdown.download(url, output, quiet=False)
-        
-        with zipfile.ZipFile(output, 'r') as z:
-            z.extractall(home + '/.deepface/weights')
-            
-    model.load_weights(model_path)
-    
-    fb_deepface_model = Model(inputs=model.layers[0].input, outputs=model.layers[-3].output)
-    
-    return fb_deepface_model
-    
+model.load_weights(model_path)
+
+fb_deepface_model = Model(inputs=model.layers[0].input, outputs=model.layers[-3].output)
+fb_deepface_model.save('./models/keras_deepface.h5')
+print('Successfully saved keras model!')
