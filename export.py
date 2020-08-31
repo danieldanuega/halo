5a1b23691dc74dcaa44476a5769f7bb8049ec4ea
Daniel Go <godaniel135@gmail.com>
2020-08-31 17:10:27 +0700

    perf: change model folder export

--- export.py
+++ export.py
@@ -3,7 +3,7 @@ import os
 from model import load_FbDeepFace
 
 MODEL_VER = "1"
-SAVE_PATH = f"{os.getcwd()}/models/FbDeepFace"
+SAVE_PATH = f"{os.getcwd()}/models/deepface"
 
 if os.path.exists(SAVE_PATH) == False:
     os.makedirs(SAVE_PATH)
